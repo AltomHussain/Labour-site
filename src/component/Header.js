3d2c09cab8a8af135a9acc454ce5caf30b0e754f
altom <altomhussian2001@yahoo.com>
2020-12-29 21:31:26 +0000

    added Navbar and Header

---
+++ src/component/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.css";
+export default function Header() {
+  return (
+    <header>
+      <div
+        style={{
+          backgroundImage: `url("https://foreignpolicy.com/wp-content/uploads/2019/09/GettyImages-1170765134.jpg?w=1536")`,
+          width: "80rem",
+        }}
+        className="jumbotron"
+      >
+        <div>
+          <h2>A New Leadership</h2>
+        </div>
+        <div>
+          <form>
+            <p>GET THE LATEST FROM LABOUR</p>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Postcode" />
+            <button type="submit" class="btn btn-danger">KEEP ME UPDATED</button>
+            <p>
+              We’ll email you about campaigns, events and opportunities to get
+              involved. You can manage your preferences or unsubscribe at any
+              time by contacting <a href = "mailto: abc@example.com">theteam@labour.org.uk.</a> Find out more about<a href="#" >how
+              we use your information.</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </header>
+  );
+}
