c1b57f5793b6029dbe81d0011483aaa0d6b7359c
altom <altomhussian2001@yahoo.com>
2020-12-30 22:09:05 +0000

    Added footer content

---
+++ src/component/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <ul>
+          <h3>ABOUT LABOUR</h3>
+          <li>Labour’s Legacy</li>
+          <li>How We Work</li>
+          <li>Diversifying Our Parliament</li>
+          <li>International Labour</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <h3>PEOPLE</h3>
+          <li>Keir Starmer</li>
+          <li>Shadow Cabinet</li>
+          <li>Local Government</li>
+          <li>Nations</li>
+          <li>Your Local Labour Team</li>
+          <li>Labour Groups</li>
+          <li>Affiliated Unions</li>
+          <li>Socialist Societies</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <h3>MEMBERS</h3>
+          <li>Conected</li>
+          <li>Why Join Labour?</li>
+          <li>New Members</li>
+          <li>Party Members</li>
+          <li>Take Part</li>
+          <li>Tools for Activists</li>
+          <li>Safeguarding</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <h3>QUICK LINKS</h3>
+          <li>Access MemberCentre</li>
+          <li>Work With Us</li>
+          <li>Press</li>
+          <li>Labour’s Shop</li>
+          <li>Annual Conference</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+        <span>
+          <strong> The Labour Party</strong>
+          Labour Central, Kings Manor<br></br>
+          Newcastle upon Tyne
+          <br></br> NE1 6PA <br></br>
+          <br></br>
+          Promoted by David Evans on behalf of the Labour Party, both at
+          Southside, 105 Victoria Street, London SW1E 6QT.
+          <br></br>
+        </span>
+      </div>
+    </footer>
+  );
+}
