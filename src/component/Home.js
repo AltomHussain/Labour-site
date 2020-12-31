0eb5a3ab3417baacb34b7b9cd021acbd9c2f296b
altom <altomhussian2001@yahoo.com>
2020-12-31 19:54:07 +0000

    Added first containet of the home page

---
+++ src/component/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="first-container">
+        <h2>See our latest campaigns</h2>
+        <div className="first-child">
+          <h4>Jobs, Jobs, Jobs</h4>
+          <p>
+            British faces a job crises: we must fight for jobs business and high
+            streets.
+          </p>
+        </div>
+        <div className="second-child">
+          <h4>Run to rebuild: Key workers standing up for their communities</h4>
+          <p>
+            Key workers you have led the way through this pandemic. You should
+            be at the heard of decision-making across the country.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
