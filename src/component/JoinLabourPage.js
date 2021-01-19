8e6c92265d0509d537ec9337bd25a495e78e1930
altom <altomhussian2001@yahoo.com>
2021-01-19 21:20:33 +0000

    Added the join page and react-router-dom

---
+++ src/component/JoinLabourPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function JoinLabourPage() {
+    return (
+        <div>
+           <img src="https://labourluton.org.uk/wp-content/uploads/2019/05/Labour_England_White.png" alt="labour-logo"/>
+        </div>
+    )
+}
