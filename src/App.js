42405bb1ad0cc8a094784b69172ac80e85f7a8ea
Altom <Altomhussain2001@yahoo.com>
2021-02-26 18:43:22 +0000

    pulled from master

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./component/Home";
 import TakeAction from "./component/TakeAction";
 
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeJoin from "./component/joinLabour/HomeJoin";
 import Volunteers from "./component/Volunteers";
 function App() {
