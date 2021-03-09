c173acb834dec4ebf06a188e6d6b0b3d7368b1c2
Altom <Altomhussain2001@yahoo.com>
2021-03-09 22:32:28 +0000

    exracted all footer info into json file to make the footer more dinamic

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,59 +1,23 @@
 import React from "react";
+import HomeFooterInfo from "./HomeFooterInfo.json"
 import "./Footer.css";
 export default function Footer() {
+
+
+  // for(const key in testing){
+  //   console.log(`${key} ${testing[key]} `);
+  // }
+  // footerInfo.map((p) => console.log(p.test));
+  console.log(HomeFooterInfo);
   return (
     <footer>
-      <div className="first-container">
-        <ul>
-          <p>ABOUT LABOUR</p>
-          <li>Labour’s Legacy</li>
-          <li>How We Work</li>
-          <li>Diversifying Our Parliament</li>
-          <li>International Labour</li>
-        </ul>
-      </div>
+    
 
-      <div className="second-container">
-        <ul>
-          <p>PEOPLE</p>
-          <li>Keir Starmer</li>
-          <li>Shadow Cabinet</li>
-          <li>Local Government</li>
-          <li>Nations</li>
-          <li>Your Local Labour Team</li>
-          <li>Labour Groups</li>
-          <li>Affiliated Unions</li>
-          <li>Socialist Societies</li>
-        </ul>
-      </div>
 
-      <div className="third-container">
-        <ul>
-          <p>MEMBERS</p>
-          <li>Conected</li>
-          <li>Why Join Labour?</li>
-          <li>New Members</li>
-          <li>Party Members</li>
-          <li>Take Part</li>
-          <li>Tools for Activists</li>
-          <li>Safeguarding</li>
-        </ul>
-      </div>
-
-      <div className="fourth-container">
-        <ul>
-          <p>QUICK LINKS</p>
-          <li>Access MemberCentre</li>
-          <li>Work With Us</li>
-          <li>Press</li>
-          <li>Labour’s Shop</li>
-          <li>Annual Conference</li>
-          <li>Contact</li>
-        </ul>
-      </div>
       <div className="fifth-container">
         <span>
-          <strong> The Labour Party</strong><br></br>
+          <strong> The Labour Party</strong>
+          <br></br>
           Labour Central, Kings Manor<br></br>
           Newcastle upon Tyne
           <br></br> NE1 6PA <br></br>
