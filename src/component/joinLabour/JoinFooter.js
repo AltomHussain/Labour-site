c7cdac5cdd77e02fea8e59df03c2eb0f98e1ae04
Altom <Altomhussain2001@yahoo.com>
2021-03-10 10:27:00 +0000

    added class names

--- src/component/joinLabour/JoinFooter.js
+++ src/component/joinLabour/JoinFooter.js
@@ -1,43 +1,66 @@
-import React from 'react'
+import React from "react";
 
 export default function JoinFooter() {
-    return (
-        <div>
-             <footer>
-            <div className="first-col">
-              <div>
-                <img src="" alt="" />
-              </div>
-              <div>
-                <a href="https://labour.org.uk/terms-and-conditions/">
-                  Terms and conditions
+  const footerInfo = {
+    "Terms and conditions": "https://labour.org.uk/terms-and-conditions/",
+    "Privacy policy": "https://www.google.co.uk/",
+    "Got a question about membership?": "https://www.google.co.uk/",
+    "Join by post": "https://www.google.co.uk/",
+  };
+  const socialMedia = {
+    Facebook: "#",
+    Twitter: "#",
+    YouTube: "#",
+    RSS: "#",
+  };
+
+  const JoinFooterInfo = Object.keys(footerInfo);
+  const displayFooterInfo = (info) => footerInfo[info];
+  const socialMediaInfo = Object.keys(socialMedia);
+  const displaySocialMediaInfo = (info) => socialMedia[info];
+
+  return (
+    <footer>
+      <div className="join-footer-container">
+        <div className="first-col">
+          <div>
+            <img src="" alt="" />
+          </div>
+
+          <div className="link-containter">
+            {JoinFooterInfo.map((term, index) => {
+              return (
+                <a key={index} href={displayFooterInfo(term)} className="links">
+                  {term}
                 </a>
-                <a href="">Privacy policy</a>
-                <a href="">Got a question about membership?</a>
-                <a href="">Join by post</a>
-              </div>
-              <div>
-                {" "}
-                <p>
-                  Powered and hosted by Tangent, a registered trading name of
-                  Tangent Marketing Services Limited, a company registered in
-                  England Wales with number 4323657, for which the registered
-                  office is at 40-44 Clipstone Street, London W1W 5DW. Promoted
-                  by David Evans on behalf of the Labour Party both at
-                  Southside, 105 Victoria Street, London SW1E 6QT. The Labour
-                  Party will place third-party cookies on your computer to help
-                  us make this website better.{" "}
-                </p>
-              </div>
-            </div>
-            <div className="second">Send us feedback</div>
-            <div className="thrid-col">
-              <p>Facebook</p>
-              <p>Twitter</p>
-              <p>YouTube</p>
-              <p>RSS</p>
-            </div>
-          </footer>
+              );
+            })}
+          </div>
+
+          <div>
+            <p className="powered">
+              Powered and hosted by Tangent, a registered trading name of
+              Tangent Marketing Services Limited, a company registered in
+              England Wales with number 4323657, for which the registered office
+              is at 40-44 Clipstone Street, London W1W 5DW. Promoted by David
+              Evans on behalf of the Labour Party both at Southside, 105
+              Victoria Street, London SW1E 6QT. The Labour Party will place
+              third-party cookies on your computer to help us make this website
+              better.
+            </p>
+          </div>
         </div>
-    )
+        <div className="feedback-container">Send us feedback</div>
+        <div className="social-container">
+          <ul>
+            {socialMediaInfo.map((term, index) => (
+              <li className="social-items" key={index}>
+                <a href={displaySocialMediaInfo(term)}>{term}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
